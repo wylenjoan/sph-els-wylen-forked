e2996871d80c607c45835d750dcfebc32716557d
Wylen Joan Lee <57890826+wylenjoan@users.noreply.github.com>
2022-10-17 16:59:42 +0800

    Put withCredentials first

--- frontend/src/apiClient/authService.ts
+++ frontend/src/apiClient/authService.ts
@@ -1,30 +1,37 @@
+import axios from "axios";
 import axiosClient from ".";
 import apiUrls from "../constants/apiUrls";
 import AuthCredentials from "../interfaces/authCredentials";
 
+axios.defaults.withCredentials = true;
+axios.defaults.xsrfCookieName = "csrftoken";
+axios.defaults.xsrfHeaderName = "X-CSRFToken";
+axios.defaults.baseURL = process.env.NODE_ENV === 'production'
+  ? apiUrls.PROD_ROOT
+  : apiUrls.DEV_ROOT;
+
 const getCsrfToken = async () => {
   return await axiosClient.get(`${apiUrls.CSRF}`);
-}
+};
 
 const loginUser = async (credentials: AuthCredentials) => {
   const csrfResponse = await getCsrfToken();
   const csrfToken = csrfResponse.data.csrfToken;
 
-  return await axiosClient.post(`${apiUrls.LOGIN}`, credentials, {
+  return await axios.post(`${apiUrls.LOGIN}`, credentials, {
     headers: {
       "X-CSRFToken": csrfToken
     },
-    withCredentials: true,
   });
-}
+};
 
 const logoutUser = async () => {
-  let response = await axiosClient.post(`${apiUrls.LOGOUT}`)
+  let response = await axiosClient.post(`${apiUrls.LOGOUT}`);
   if (response.status === 200) {
     localStorage.removeItem('user');
     window.location.reload();
   }
-}
+};
 
 
 export {
--- frontend/src/apiClient/index.ts
+++ frontend/src/apiClient/index.ts
@@ -1,17 +1,18 @@
 import axios from "axios";
 import apiUrls from "../constants/apiUrls";
 
+axios.defaults.withCredentials = true;
+
 const axiosClient = axios.create({
+  withCredentials: true,
   xsrfHeaderName: 'X-CSRFToken',
   xsrfCookieName: 'csrftoken',
-  withCredentials: true,
   headers: {
     "Accept": "application/json",
     "Content-Type": "application/json",
   },
 });
 
-axiosClient.defaults.withCredentials = true;
 axiosClient.defaults.baseURL = process.env.NODE_ENV === 'production'
   ? apiUrls.PROD_ROOT
   : apiUrls.DEV_ROOT;
